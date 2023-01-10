ead7cf99fd7d65a40dcb83364802d74d03636d58
Umesh Chauhan <umeshchauhan92@hotamil.com>
2023-01-10 13:35:59 +0530

    Changed validations with annotations and updated swagger doc

--- src/main/java/com/order/ecommerce/controller/OrderController.java
+++ src/main/java/com/order/ecommerce/controller/OrderController.java
@@ -1,12 +1,8 @@
 package com.order.ecommerce.controller;
 
-import com.order.ecommerce.dto.OrderResponseDto;
-import com.order.ecommerce.dto.OrderDto;
-import com.order.ecommerce.service.IOrderService;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,68 +11,91 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-@Slf4j
+import com.order.ecommerce.dto.ErrorDto;
+import com.order.ecommerce.dto.OrderDto;
+import com.order.ecommerce.dto.OrderResponseDto;
+import com.order.ecommerce.service.IOrderService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/orders")
 public class OrderController {
 
-    private final IOrderService orderService;
+	private final IOrderService orderService;
 
-    /**
-     * Creates order
-     * @param orderDto
-     * @return
-     */
-    @PostMapping
-    @Operation(summary = "Create an order", description = "Create an order")
-    public OrderResponseDto createOrder(@RequestBody OrderDto orderDto) {
-        validateArgument(orderDto);
-        return orderService.createOrder(orderDto);
-    }
+	/**
+	 * Creates order
+	 * 
+	 * @param orderDto
+	 * @return
+	 */
+	@PostMapping
+	@Operation(summary = "Create an order", description = "Create an order")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Order created",
+					content = @Content(schema = @Schema(implementation = OrderResponseDto.class))),
+			@ApiResponse(responseCode = "400", description = "Bad Request",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Exception",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class)))
+	})
+	public OrderResponseDto createOrder(@Valid @RequestBody OrderDto orderDto) {
+		return orderService.createOrder(orderDto);
+	}
 
-    /**
-     * Finds Order by Id
-     * @param orderId
-     * @return
-     */
-    @GetMapping("/{orderId}")
-    @Operation(summary = "Find order", description = "Find order by id")
-    public OrderDto findOrderBy(@PathVariable(name = "orderId") String orderId) {
-        validateArgument(orderId == null || orderId.isEmpty(), "order id cannot be null or empty");
-        return orderService.findOrderById(orderId);
-    }
+	/**
+	 * Finds Order by Id
+	 * 
+	 * @param orderId
+	 * @return
+	 */
+	@GetMapping("/{orderId}")
+	@Operation(summary = "Find order", description = "Find order by id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Order found",
+					content = @Content(schema = @Schema(implementation = OrderDto.class))),
+			@ApiResponse(responseCode = "400", description = "Bad Request",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+			@ApiResponse(responseCode = "404", description = "Order not found",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Exception",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class)))
+	})
+	public OrderDto findOrderBy(
+			@NotEmpty(message = "Order Id cannot be null or empty") @PathVariable(name = "orderId") String orderId) {
+		return orderService.findOrderById(orderId);
+	}
 
-    /**
-     * Updates order status
-     * @param orderId
-     * @param orderStatus
-     */
-    @PatchMapping("/{orderId}")
-    @Operation(summary = "Update order status", description = "Update order status")
-    public void updateOrderStatus(@PathVariable("orderId") String orderId,
-                                  @RequestParam(name = "orderStatus") String orderStatus) {
-        validateArgument(orderId == null || orderId.isEmpty(), "order id cannot be null or empty");
-        validateArgument(orderStatus == null || orderStatus.isEmpty(), "order status cannot be null or empty");
-        orderService.updateOrderStatus(orderId, orderStatus);
-    }
-
-    private void validateArgument(OrderDto orderDto) {
-        validateArgument(orderDto.getCustomerId() == null || orderDto.getCustomerId().isEmpty(), "customer id cannot be null or empty");
-        validateArgument(orderDto.getTitle() == null || orderDto.getTitle().isEmpty(), "title cannot be null or empty");
-        validateArgument(orderDto.getPaymentMode() == null || orderDto.getPaymentMode().isEmpty(), "payment mode cannot be null or empty");
-        validateArgument(orderDto.getBillingAddress() == null, "billing address cannot be null");
-        validateArgument(orderDto.getOrderItems() == null || orderDto.getOrderItems().isEmpty(), "order items cannot be null or empty");
-        validateArgument(orderDto.getOrderStatus() == null || orderDto.getOrderStatus().isEmpty(), "order status cannot be null or empty");
-    }
-
-    private void validateArgument(boolean condition, String message) {
-        if (condition) {
-            log.error("Error while processing request with message = {}", message);
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, message);
-        }
-    }
+	/**
+	 * Updates order status
+	 * 
+	 * @param orderId
+	 * @param orderStatus
+	 */
+	@PatchMapping("/{orderId}")
+	@Operation(summary = "Update order status", description = "Update order status")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Update Successful"),
+			@ApiResponse(responseCode = "400", description = "Bad Request",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+			@ApiResponse(responseCode = "404", description = "Order not found",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Exception",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class)))
+	})
+	public void updateOrderStatus(
+			@NotEmpty(message = "Order Id cannot be null or empty") @PathVariable("orderId") String orderId,
+			@NotEmpty(message = "Order Status cannot be null or empty") @RequestParam(
+					name = "orderStatus") String orderStatus) {
+		orderService.updateOrderStatus(orderId, orderStatus);
+	}
 
 }
--- src/main/java/com/order/ecommerce/controller/ProductController.java
+++ src/main/java/com/order/ecommerce/controller/ProductController.java
@@ -1,63 +1,74 @@
 package com.order.ecommerce.controller;
 
-import com.order.ecommerce.dto.ProductDto;
-import com.order.ecommerce.service.IProductService;
-import io.swagger.v3.oas.annotations.Operation;
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
+import javax.validation.Valid;
+import javax.validation.constraints.NotEmpty;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
-@Slf4j
+import com.order.ecommerce.dto.ErrorDto;
+import com.order.ecommerce.dto.ProductDto;
+import com.order.ecommerce.service.IProductService;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import lombok.RequiredArgsConstructor;
+
 @RestController
 @RequiredArgsConstructor
 @RequestMapping("/api/v1/products")
 public class ProductController {
 
-    private final IProductService productService;
+	private final IProductService productService;
 
-    /**
-     * Creates a product
-     * @param productDto
-     * @return
-     */
-    @PostMapping
-    @Operation(summary = "Create a product", description = "Create a product")
-    public ProductDto createProduct(@RequestBody ProductDto productDto) {
-        validateArgument(productDto);
-        return productService.createProduct(productDto);
-    }
+	/**
+	 * Creates a product
+	 * 
+	 * @param productDto
+	 * @return
+	 */
+	@PostMapping
+	@Operation(summary = "Create a product", description = "Create a product")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Product created",
+					content = @Content(schema = @Schema(implementation = ProductDto.class))),
+			@ApiResponse(responseCode = "400", description = "Bad Request",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Exception",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class)))
+	})
+	public ProductDto createProduct(@Valid @RequestBody ProductDto productDto) {
+		return productService.createProduct(productDto);
+	}
 
-    /**
-     * Finds product by id
-     * @param productId
-     * @return
-     */
-    @GetMapping("/{productId}")
-    @Operation(summary = "Find a product", description = "Find a product by id")
-    public ProductDto findProductById(@PathVariable(name = "productId") String productId) {
-        validateArgument(productId == null || productId.isEmpty(), "Product Id cannot be null or empty");
-        return productService.findProductById(productId);
-    }
+	/**
+	 * Finds product by id
+	 * 
+	 * @param productId
+	 * @return
+	 */
+	@GetMapping("/{productId}")
+	@Operation(summary = "Find a product", description = "Find a product by id")
+	@ApiResponses(value = {
+			@ApiResponse(responseCode = "200", description = "Product found",
+					content = @Content(schema = @Schema(implementation = ProductDto.class))),
+			@ApiResponse(responseCode = "400", description = "Bad Request",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+			@ApiResponse(responseCode = "404", description = "Product not found",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class))),
+			@ApiResponse(responseCode = "500", description = "Internal Server Exception",
+					content = @Content(schema = @Schema(implementation = ErrorDto.class)))
+	})
+	public ProductDto findProductById(@NotEmpty(message = "Product Id cannot be null or empty") @PathVariable(
+			name = "productId") String productId) {
+		return productService.findProductById(productId);
+	}
 
-    private void validateArgument(ProductDto productDto) {
-        validateArgument(productDto == null, "Product cannot be null");
-        validateArgument(productDto.getProductId() == null || productDto.getProductId().isEmpty(), "Product Id cannot be null or empty");
-        validateArgument(productDto.getSku() == null || productDto.getSku().isEmpty(), "Product Sku cannot be null or empty");
-        validateArgument(productDto.getTitle() == null || productDto.getTitle().isEmpty(), "Product Title cannot be null");
-        validateArgument(productDto.getDescription() == null || productDto.getDescription().isEmpty(), "Product Description cannot be empty");
-    }
-
-    private void validateArgument(boolean condition, String message) {
-        if (condition) {
-            log.error("Error while processing request with message = {}", message);
-            throw new ResponseStatusException(HttpStatus.BAD_REQUEST, message);
-        }
-    }
 }
